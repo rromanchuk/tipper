d281eb332a7baba61343397e59e8728391c09c30
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-08 20:38:11 -0700

    Cognito support

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,11 @@
+
+module Api
+  require "application_responder"
+  class BaseController < ActionController::Base
+    self.responder = ApplicationResponder
+    respond_to :json
+
+    protect_from_forgery with: :null_session
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, path: '/', defaults: { format: 'json' } do
+     resource :me,                  only: [:show], controller: 'me'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
