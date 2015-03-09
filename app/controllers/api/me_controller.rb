4f1378f111d1bf2a96ae835c26207fc983ba173e
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-09 00:24:12 -0700

    Add correct praram

--- app/controllers/api/me_controller.rb
+++ app/controllers/api/me_controller.rb
@@ -9,7 +9,7 @@ module Api
         identity_pool_id: "us-east-1:71450ec4-894b-4e51-bfbb-35a012b5b514",
         identity_id: nil,
         # required
-        logins: { "com.ryanromanchuk.tipper" => "blahblah" },
+        logins: { "com.ryanromanchuk.tipper" => params[:token] },
         token_duration: 1,)
 
       render json: {token: resp.token, identity_id: resp.identity_id}
