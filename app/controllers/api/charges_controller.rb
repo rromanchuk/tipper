0d6b8f9e805e9d5ebf4a5a3b4d8f3c2cf4aecb3b
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-11 23:58:23 -0700

    bug fix

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -1,26 +1,28 @@
-class ChargesController < Api::BaseController
-  def new
-  end
+module Api
+  class ChargesController < Api::BaseController
+    def new
+    end
 
-  def create
-    # Amount in cents
-    @amount = 500
+    def create
+      # Amount in cents
+      @amount = 500
 
-    customer = Stripe::Customer.create(
-      :card  => params[:stripeToken]
-    )
+      customer = Stripe::Customer.create(
+        :card  => params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Tipper refill',
-      :currency    => 'usd'
-    )
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Tipper refill',
+        :currency    => 'usd'
+      )
 
-    respond_with charge
+      respond_with charge
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to charges_path
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to charges_path
+    end
   end
 end
