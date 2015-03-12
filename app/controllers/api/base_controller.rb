79dce7833025f95c3bcdd6c68f2fc89b75ae9dfa
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-12 00:13:55 -0700

    More cleanup

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -5,7 +5,7 @@ module Api
     self.responder = ApplicationResponder
     respond_to :json
 
-    protect_from_forgery with: :null_session
+    #protect_from_forgery with: :null_session
 
   end
 end
