a4124c5e94b28f9aa9b050f13e14dcf11f5e7546
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-15 19:11:57 -0700

    Fix fatal

--- app/controllers/api/me_controller.rb
+++ app/controllers/api/me_controller.rb
@@ -12,11 +12,10 @@ module Api
         logins: { "com.ryanromanchuk.tipper" => username },
         token_duration: 1)
 
-      render json: {token: resp.token,
-        identity_id: resp.identity_id,
-        bitcoin_address: B.addressForTwitterUsername(username),
-        bitcoin_balance: B.balance(username) }
+      render json: {token: resp.token, identity_id: resp.identity_id, bitcoin_address: B.addressForTwitterUsername(username), bitcoin_balance: B.balance(username) }
+    end
 
+    
     private
     def identity
       @cognitoidentity ||= Aws::CognitoIdentity::Client.new(region: 'us-east-1', credentials: Aws::SharedCredentials.new)
