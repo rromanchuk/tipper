315112c7bd35e511fd8491c75925bffe054b4a2a
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-15 17:19:06 -0700

    Report balances

--- app/controllers/api/b_controller.rb
+++ app/controllers/api/b_controller.rb
@@ -1,14 +1,13 @@
 module Api
   class BController < Api::BaseController
     def balance
-      B.client.balance(params[:username])
-      render json: {balance: 0}
+      balance = B.client.balance(params[:username])
+      render json: {balance: balance}
     end
 
     def address
-      B.client
-      render json: {balance: 0}
-
+      address = B.addressForTwitterUsername(params[:username])
+      render json: {bitcoin_address: address}
     end
   end
 end
