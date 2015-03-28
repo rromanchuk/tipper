537b039cee4e2425a27ad3946c94606c6401aa8a
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-28 01:18:33 -0700

    Updates

---
+++ Procfile
@@ -0,0 +1,2 @@
+fetch_favorites: rails r lib/fetch_favorites_worker.rb
+favorite_stream: rails r lib/t.rb
---
+++ lib/sqs_queues.rb
@@ -0,0 +1,9 @@
+class SQSQueues
+  def self.fetch_favorites
+    "***REMOVED***"
+  end
+
+  def self.new_tip
+    "***REMOVED***"
+  end
+end
