3562df797ed8a94521dd5bdb060123e39bc7ae16
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-29 21:38:24 -0700

    Debug

--- lib/t.rb
+++ lib/t.rb
@@ -38,10 +38,12 @@ EventMachine.run {
         puts "Falling behind!"
       when Twitter::Streaming::Event
         puts "Found a favorite event..."
-        puts "Source #{object.source.id}, Target #{object.target.id}, object #{object.target_object.id}"
+        puts "currentUser: #{user["TwitterUserID"]},  Source #{object.source.id}, Target #{object.target.id}, object #{object.target_object.id}"
         if object.source.id == user["TwitterUserID"]
           publish_new_tweet(user)
           sqs.send_message(queue_url: SQSQueues.new_tip, message_body: { "TweetID": object.target_object.id, "FromTwitterID": object.source.id, "ToTwitterID": object.target.id }.to_json )
+        else
+          puts "Skipping..."
         end
       end
     end
