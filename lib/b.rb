369c6ac9b9cd9c78acdf3536bd6d24def5286cea
Ryan Romanchuk <rromanchuk@gmail.com>
2015-03-29 19:21:05 -0700

    Allow 0 confirmations

--- lib/b.rb
+++ lib/b.rb
@@ -10,7 +10,7 @@ class B
   end
 
   def self.balance(address)
-    balance = client.getreceivedbyaddress(address)
+    balance = client.getreceivedbyaddress(address, 0)
     s = Satoshi.new(balance)
     {satoshi: s.to_i, mbtc: s.to_mbtc, btc: s.to_btc}
   end
@@ -24,20 +24,34 @@ class B
   end
 
   def self.tip_user(fromAddress, toAddress)
+    # Get the total avail inputs from the snders address with at least 1 confirmation
     unspents = client.listunspent(1, 9999999, [fromAddress])
     puts "unspents: #{unspents}"
+
+    # Sum all of the amounts
     amounts_array = unspents.map {|a| a["amount"] }
     puts "amounts_array: #{amounts_array}"
     senderBTCBalance = amounts_array.inject(:+)
     puts "senderBTCBalance: #{senderBTCBalance}"
+
+    # The amount of btc to send to the receiving user
     amount_to_send_to_other_user = 0.001
+
+    # The transaction fee to include for the transaction
     transaction_fee = 0.0001
+
+    # Calculate the amount that needs to be sent back to the sender after using avail inputs
     amount_to_send_back_to_self = senderBTCBalance - amount_to_send_to_other_user - transaction_fee
 
+    # Generate the transaction
     rawtx = client.createrawtransaction(unspents, {fromAddress=>amount_to_send_back_to_self, toAddress => amount_to_send_to_other_user})
     puts "rawtx:#{rawtx}"
+
+    # Sign the transaction
     signedTx = client.signrawtransaction(rawtx, unspents)
     puts "signedTx: #{signedTx}"
+
+    # Broadcast transaction on the network
     return client.sendrawtransaction(signedTx["hex"])
   end
 
