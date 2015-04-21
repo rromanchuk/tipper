036f66b44d0265e3bab874cd932b9428abd9d280
Ryan Romanchuk <rromanchuk@gmail.com>
2015-04-21 08:25:38 -0700

    Adjust models

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -6,9 +6,41 @@ class Tip
       table_name: "TipperTips",
     )
   end
-
-  def self.new_tip(tweetId, from, to, txid)
+  def self.new_tip(tweet, fromUser, toUser, txid)
     puts "new_tip tweetId:#{tweetId}, from:#{from}, to:#{to}, txid:#{txid}"
+    resp = db.update_item(
+      table_name: "TipperTwitterFavorites",
+      key: {
+        "TweetID" => tweet.id.to_s,
+      },
+      attribute_updates: {
+        "TipperUserID" => {
+          value: fromUser["TwitterUserID"]
+        },
+        "ToTwitterID": {
+          value: toUser["TwitterUserID"]
+        },
+        "FromTwitterID": {
+          value: fromUser["TwitterUserID"]
+        },
+        "FromTwitterUsername": {
+          value: fromUser["TwitterUsername"]
+        }
+        "txid" => {
+          value: txid
+        },
+        "CreatedAt" => {
+          value: Time.now.to_i
+        },
+        "TweetJSON" => {
+          value: tweet.to_json
+        },
+        "DidLeaveTip" => {
+          value: true
+        }
+      },
+    )
+
     resp = db.update_item(
       table_name: "TipperTips",
       key: {
@@ -26,6 +58,9 @@ class Tip
         },
         "CreatedAt" => {
           value: Time.now.to_i
+        },
+        "TweetJSON" => {
+          value: tweetJSON
         }
       },
     )
