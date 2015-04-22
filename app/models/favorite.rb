5b730fff94b6405fa42db2d02d543e4dfa1d02a0
Ryan Romanchuk <rromanchuk@gmail.com>
2015-04-22 15:36:51 -0700

    new modeling

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,19 +3,24 @@ class Favorite
   def self.batchWrite(collection, currentUser)
     putRequests = []
     collection.each do |tweet|
-      putRequests << { put_request: { item: { "TweetID": "#{currentUser["TwitterUserID"]}-#{tweet.id.to_s}", 
-        "TweetJSON": tweet.to_json, 
-        "CreatedAt": tweet.created_at.to_i, 
-        "TipperUserID": currentUser["TwitterUserID"],
-        "FromTwitterID": currentUser["TwitterUserID"],
-        "FromTwitterUsername": currentUser["TwitterUsername"],
-        "ToTwitterID": tweet.user.id.to_s,
-        "ToTwitterUsername": tweet.user.screen_name } } }
+      putRequests << { put_request:
+        { item:
+          { "TweetID": tweet.id.to_s,
+            "TweetJSON": tweet.to_json, 
+            "CreatedAt": tweet.created_at.to_i, 
+            "TipperUserID": currentUser["TwitterUserID"],
+            "FromTwitterID": currentUser["TwitterUserID"],
+            "FromTwitterUsername": currentUser["TwitterUsername"],
+            "ToTwitterID": tweet.user.id.to_s,
+            "ToTwitterUsername": tweet.user.screen_name 
+          } 
+        } 
+      }
     end
     resp = db.batch_write_item(
       # required
       request_items: {
-        "TipperTwitterFavorites" => putRequests
+        "TipperTwitterFavoritesTest" => putRequests
       },
     )
   end
