821c5c895f06e457d7b65e07b0de5896bfc9c24a
Ryan Romanchuk <rromanchuk@gmail.com>
2015-05-04 10:42:32 -0700

    Dead folder

--- lib/sqs_queues.rb
+++ lib/sqs_queues.rb
@@ -6,4 +6,8 @@ class SQSQueues
   def self.new_tip
     "https://sqs.us-east-1.amazonaws.com/080383581145/TipperNewTip"
   end
+
+  def self.wallet_notify
+    "https://sqs.us-east-1.amazonaws.com/080383581145/WalletNotify"
+  end
 end
