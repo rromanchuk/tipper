1a2e9dcc2dc39297ba4147ffeb94c1aea12edb93
Ryan Romanchuk <rromanchuk@gmail.com>
2015-05-04 10:42:55 -0700

    New process for walletnotify events

---
+++ app/models/transaction.rb
@@ -0,0 +1,38 @@
+class Transaction
+  TABLE_NAME = "TipperBitcoinTransactions"
+
+  def self.create(txid)
+    transaction = B.client.gettransaction(txid)
+    attributes = {
+        "amount" => {
+          value: transaction["amount"]
+        },
+        "fee" => {
+          value: transaction["fee"]
+        },
+        "time" => {
+          value: transaction["time"]
+        }
+      }
+
+    resp = db.update_item(
+      table_name: TABLE_NAME,
+      key: {
+        "txid" => txid,
+      },
+      attribute_updates: attributes,
+      return_values: "ALL_NEW"
+    )
+    resp.attributes
+  end
+
+
+  def self.db
+    @dynamodb ||= Aws::DynamoDB::Client.new(region: 'us-east-1', credentials: Aws::SharedCredentials.new)
+  end
+  
+  def db
+    Transaction.db
+  end
+
+end
