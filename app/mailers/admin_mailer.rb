d712e12282106a0149f9f4f628516286f470512c
Ryan Romanchuk <rromanchuk@gmail.com>
2015-05-17 19:51:23 -0700

    Cleanup

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ApplicationMailer
   
   def wallet_notify(tx)
     @tx = tx
-    mail(to: ['***REMOVED***', 'marcus.siegel@gmail.com'], subject: "WALLET EVENT: Amount in/out: #{tx["amount"]}, Fees: #{tx["fee"]}")
+    mail(to: ['***REMOVED***', 'marcus.siegel@gmail.com'], subject: "#{tx["category"]}: #{tx["amount"]}, Fees: #{tx["fee"]}")
   end
 
 end
