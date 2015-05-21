249ebccc3887d67aa4e44b95ca273c15c5a5fd46
Ryan Romanchuk <rromanchuk@gmail.com>
2015-05-20 20:45:02 -0700

    fixes

--- lib/t.rb
+++ lib/t.rb
@@ -27,7 +27,7 @@ def publish_new_tweet(user)
       message: {"default" => "Received a favorite from tweet stream", "APNS_SANDBOX": apns_payload }.to_json
     )
   rescue Aws::SNS::Errors::EndpointDisabled
-    Rails.logger.error "Aws::SNS::Errors::EndpointDisabled"
+    logger.error "Aws::SNS::Errors::EndpointDisabled"
   end
 end
 
@@ -36,46 +36,43 @@ EventMachine.run {
     next unless user["IsActive"] == "X"
     logger.info "Starting stream for user #{user["TwitterUsername"]}"
 
-    begin
-      client = Twitter::Streaming::Client.new do |config|
-        config.consumer_key        = "oGbPqpQeXUojn7macV7Ze9HvO"
-        config.consumer_secret     = "iJDZtadyNK6BwXB49xszyBI6y748iERGEmUQM3veXNlcmKzqwJ"
-        config.access_token        = user["TwitterAuthToken"]
-        config.access_token_secret = user["TwitterAuthSecret"]
-      end
-    rescue Twitter::Error::Unauthorized
-      logger.info "User #{user["TwitterUsername"]} has invalid token"
-      next
+    client = Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = "oGbPqpQeXUojn7macV7Ze9HvO"
+      config.consumer_secret     = "iJDZtadyNK6BwXB49xszyBI6y748iERGEmUQM3veXNlcmKzqwJ"
+      config.access_token        = user["TwitterAuthToken"]
+      config.access_token_secret = user["TwitterAuthSecret"]
     end
 
-  EventMachine.defer {
-    begin
-      client.user(with: "user") do |object|
-        case object
-        when Twitter::Tweet
-          puts "It's a tweet!"
-        when Twitter::DirectMessage
-          puts "It's a direct message!"
-        when Twitter::Streaming::StallWarning
-          puts "Falling behind!"
-        when Twitter::Streaming::Event
-          logger.info "Found event: #{object.name}"
-          if object.name == :favorite
-            Rails.logger.info "name: #{object.name}, currentUser: #{user["TwitterUserID"]},  Source #{object.source.id}, Target #{object.target.id}, object #{object.target_object.id}"
-            if object.source.id.to_s == user["TwitterUserID"] && object.source.id.to_s != object.target.id.to_s
-              publish_new_tweet(user)
-              sqs.send_message(queue_url: SqsQueues.new_tip, message_body: { "TweetID": object.target_object.id.to_s, "FromTwitterID": object.source.id.to_s, "ToTwitterID": object.target.id.to_s }.to_json )
-            else
-              puts "Skipping..."
+    EventMachine.defer {
+      begin
+        client.user(with: "user") do |object|
+          case object
+          when Twitter::Tweet
+            puts "It's a tweet!"
+          when Twitter::DirectMessage
+            puts "It's a direct message!"
+          when Twitter::Streaming::StallWarning
+            logger.info "Falling behind!"
+          when Twitter::Streaming::Event
+            logger.info "Found event: #{object.name}"
+            if object.name == :favorite
+              Rails.logger.info "name: #{object.name}, currentUser: #{user["TwitterUserID"]},  Source #{object.source.id}, Target #{object.target.id}, object #{object.target_object.id}"
+              if object.source.id.to_s == user["TwitterUserID"] && object.source.id.to_s != object.target.id.to_s
+                publish_new_tweet(user)
+                sqs.send_message(queue_url: SqsQueues.new_tip, message_body: { "TweetID": object.target_object.id.to_s, "FromTwitterID": object.source.id.to_s, "ToTwitterID": object.target.id.to_s }.to_json )
+              else
+                logger.info "Skipping..."
+              end
             end
           end
         end
+      rescue Twitter::Error::Unauthorized => e
+        logger.info "User #{user["TwitterUsername"]} has invalid token"
+        Bugsnag.notify(e, {:severity => "error"})
+        next
       end
-    rescue => e
-      Bugsnag.notify(e, {:severity => "error"})
-    end
-  }
-end
+    }
+  end
 }
 
 
