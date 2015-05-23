0e633aebb12223c8c4019e17f39728b58d367f9e
Ivan Kataitsev <ivan.kataitsev@gmail.com>
2015-05-23 12:09:33 +0300

    Fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rbenv_ruby, '2.2.2'
 set :rbenv_map_bins, %w{rake gem bundle ruby rails leye}
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
+set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', '.eye')
