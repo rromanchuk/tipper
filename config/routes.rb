c99a2ff930abfbbf7a744ce7325a0723f0957b28
Ryan Romanchuk <rromanchuk@gmail.com>
2015-05-25 02:20:46 -0700

    Better mailers

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,10 @@ Rails.application.routes.draw do
     end
   end
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/devel/emails"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
