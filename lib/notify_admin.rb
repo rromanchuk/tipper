3d14da3116ac05b8683a907a275857de430d292f
Ryan Romanchuk <rromanchuk@gmail.com>
2015-07-11 10:46:20 -0500

    test

---
+++ lib/notify_admin.rb
@@ -0,0 +1,28 @@
+class NotifyAdmin
+  TOPIC = "arn:aws:sns:us-east-1:080383581145:AdminNotification"
+  
+  def self.new_user(username)
+    message =  "New user: #{username}"
+    apns_payload = { "aps" => { "alert" => message, "badge" => 1 } }.to_json
+    resp = sns.publish(
+      topic_arn: TOPIC,
+      message_structure: "json",
+      message: {"default" => message, "APNS_SANDBOX": apns_payload, "APNS": apns_payload, "sms": message }.to_json
+    )
+  end
+
+  def self.external_deposit
+
+  end
+
+  def self.withdrawl
+
+  end
+
+  private
+
+  def self.sns
+     @sns ||= Aws::SNS::Client.new(region: 'us-east-1', credentials: Aws::SharedCredentials.new)
+  end
+
+end
