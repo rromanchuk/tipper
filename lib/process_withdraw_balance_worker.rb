6c12283a217b0140619e6651541ba422adb92566
Ryan Romanchuk <rromanchuk@gmail.com>
2015-07-29 10:54:24 -0700

    quiet logs

--- lib/process_withdraw_balance_worker.rb
+++ lib/process_withdraw_balance_worker.rb
@@ -61,9 +61,9 @@ class ProcessWithdrawBalanceWorker
     #test_event
     EventMachine.run do
       EM.add_periodic_timer(25.0) do
-        logger.info "Ready to process tasks.."
+        #logger.info "Ready to process tasks.."
         messages = receive
-        logger.info "Found message #{messages}"
+        #logger.info "Found message #{messages}"
         process_messages(messages)
       end
     end
