62987131db9f972a7339c6781ad9f93dd1f810ca
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-18 20:14:28 -0700

    fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,7 @@ class IndexController < ApplicationController
   def index
     rev = Redis.current.lindex("tipper", 0)
     index_text = Redis.current.get(rev)
-    Rails.log.info session.inspect
+    Rails.logger.info session.inspect
     #index_text = index_text.gsub('CSRF-TOKEN')
     render text: index_text
   end
