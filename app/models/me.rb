153a793f255b4c2fac8b5b820eb8187251ef42ce
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-19 14:54:00 -0700

    add an id attribute

--- app/models/me.rb
+++ app/models/me.rb
@@ -5,11 +5,12 @@ class Me
   include ActiveModel::Model
   
   def initialize(user_from_dynamo={})
-    @user_id = user_from_dynamo["UserID"]
+    @id = user_from_dynamo["UserID"]
     @bitcoin_address = user_from_dynamo["BitcoinAddress"]
     @twitter_user_id = user_from_dynamo["TwitterUserID"]
     @twitter_username = user_from_dynamo["TwitterUsername"]
     @profile_image = user_from_dynamo["ProfileImage"]
+    @user_id = user_from_dynamo["UserID"]
   end
 
   def as_json(options={})
