1e844855ed884a36b3a3df84a30c37497f80b4fa
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-20 18:48:50 -0700

    fix sms action

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,7 +3,7 @@ class SmsController < ApplicationController
   
   def download
     to_number = Phony.normalize(params[:sms][:to])
-    twilio.account.messages.create({from: '+16504198197', to: to_number, body: body})
+    twilio.account.messages.create({from: from, to: to_number, body: body})
     render json: {'sms': {body: body, id: to_number, to: to_number, from: from}}
   end
 
@@ -11,6 +11,10 @@ class SmsController < ApplicationController
 
   def body
     @body ||= 'Download tipper now from https://itunes.apple.com/us/app/tipper-micro-payments/id986575823'
+  end
+
+  def from
+    @from ||= '+16504198197'
   end
 
   def twilio
