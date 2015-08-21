4113ff8cc052ef8a19da6f306eb804439563e6eb
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-21 12:44:57 -0700

    Don't require authentication

--- app/controllers/api/tips_controller.rb
+++ app/controllers/api/tips_controller.rb
@@ -1,12 +1,9 @@
 module Api
   class TipsController < Api::BaseController
+    skip_before_filter :require_user!
+
     def show
       render json: Tip.find_by_txid(params[:id])
     end
-
-    # def txid
-    #   params.require(:twitter_auth_secret)
-    # end
-
   end
 end
