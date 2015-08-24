4dd9e92c6e74b1e22dbcb81e30e52b9f3cbf9ee2
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-23 22:16:06 -0700

    Add user show route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < Api::BaseController
+  skip_before_filter :require_user!
   def show
-    render json: {}
+    render json: {user: User.new(user)}
   end
 
   def disconnect
     render json: {}
+  end
+
+  protected
+  def user
+    @user ||= User.find(params[:id])
   end
 end
