e3dbf61d3f30dede82eb27c81ef31b7f24470ce9
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-23 22:23:29 -0700

    Fix controller namespace

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,17 @@
-class UsersController < Api::BaseController
-  skip_before_filter :require_user!
-  def show
-    render json: {user: User.new(user)}
-  end
+module Api
+  class UsersController < Api::BaseController
+    skip_before_filter :require_user!
+    def show
+      render json: {user: User.new(user)}
+    end
 
-  def disconnect
-    render json: {}
-  end
+    def disconnect
+      render json: {}
+    end
 
-  protected
-  def user
-    @user ||= User.find(params[:id])
+    protected
+    def user
+      @user ||= User.find(params[:id])
+    end
   end
 end
