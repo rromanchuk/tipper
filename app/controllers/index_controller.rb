e82f777535d59b0f2f89b85fdde813221f8cd988
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-26 16:02:06 -0700

    cleanup index controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,9 +1,27 @@
 class IndexController < ApplicationController
   def index
-    rev = Redis.current.lindex("tipper", 0)
-    index_text = Redis.current.get(rev)
-    Rails.logger.info form_authenticity_token
+    check_revision
     index_text = index_text.gsub('CSRF-TOKEN', form_authenticity_token)
     render text: index_text
+  end
+
+  private
+  def check_revision
+    if rev != current_rev
+      @current_rev = rev
+      @index_text = Redis.current.get(current_rev)
+    end
+  end
+
+  def rev
+    Redis.current.lindex("tipper", 0)
+  end
+
+  def current_rev
+    @current_rev
+  end
+
+  def index_text
+    @index_text ||= Redis.current.get(rev)
   end
 end
