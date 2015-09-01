61b5164f85363447ec7cbfad087e15fa4d3607ff
Ryan Romanchuk <rromanchuk@gmail.com>
2015-08-31 23:56:26 -0700

    organization

---
+++ app/controllers/api/sms_controller.rb
@@ -0,0 +1,25 @@
+require 'twilio-ruby'
+module Api
+  class SmsController < ApplicationController
+    
+    def download
+      to_number = Phony.normalize(params[:sms][:to])
+      twilio.account.messages.create({from: from, to: to_number, body: body})
+      render json: {'sms': {body: body, id: to_number, to: to_number, from: from}}
+    end
+
+    private
+
+    def body
+      @body ||= 'Download tipper now from https://itunes.apple.com/us/app/tipper-micro-payments/id986575823'
+    end
+
+    def from
+      @from ||= '+16504198197'
+    end
+
+    def twilio
+      @twilio ||= Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+    end
+  end
+end
