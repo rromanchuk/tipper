df588b08a94e5e473df7e5c80c0d612f76a78ef1
Ryan Romanchuk <rromanchuk@gmail.com>
2015-09-14 11:30:26 -0700

    Start adding notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,18 +1,16 @@
 class Notification
   include EmberModel
   TABLE_NAME = "TipperNotifications"
+  #TYPES = {:transaction_confirmed: }
 
 
   UPDATE_EXPRESSION = "SET " +
-                      "UserID = :user_id, " +
-                      "TwitterAuthToken = :twitter_auth_token, " +
-                      "TwitterAuthSecret = :twitter_auth_secret, " +
-                      "ProfileImage = :profile_image, " +
-                      "TwitterUserID = :twitter_user_id, " +
-                      "TwitterUsername = :twitter_username"
+                      "Type = :type, " +
+                      "Text = :text, " +
+                      "CreatedAt = :created_at " +
 
 
-  def self.create(user, type)
+  def self.create(user_id, type, text)
     resp = db.update_item(
         # required
         table_name: Notification::TABLE_NAME,
@@ -20,9 +18,8 @@ class Notification
         key: {
           "UserID" => user_id,
         },
-        update_expression: update_expression,
-        expression_attribute_names: expression_attribute_names,
-        expression_attribute_values: update_values,
+        update_expression: UPDATE_EXPRESSION,
+        expression_attribute_values: {":type": type, ":text": text, ":created_at": Time.now.to_i},
         return_values: "ALL_NEW")
 
     resp.attributes
