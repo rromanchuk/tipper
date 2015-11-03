694e1eaaa3a7e16826fc8de1b6787bae6c2b7f5c
Ryan Romanchuk <rromanchuk@gmail.com>
2015-11-03 10:51:22 -0800

    testing

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,8 @@
+lowlevel_error_handler do |e|
+  Rollbar.critical(e)
+  [500, {}, ["An error has occurred, and engineers have been informed. Please reload the page. If you continue to have problems, contact support@example.com\n"]]
+end
+
 if ENV['RAILS_ENV'] == 'production'
   directory "/home/ec2-user/apps/tipper/current"
   workers 2
@@ -12,3 +17,5 @@ threads 1, 1
 environment ENV['RAILS_ENV'] || 'development'
 
 prune_bundler
+
+
