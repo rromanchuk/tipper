1bb346b34f7fbf378de471e147b3dbb23829efb3
Ryan Romanchuk <rromanchuk@gmail.com>
2015-11-04 10:59:14 -0800

    back to defaults

--- config/puma.rb
+++ config/puma.rb
@@ -1,24 +1,14 @@
-# lowlevel_error_handler do |e|
-#   Rollbar.critical(e)
-#   [500, {}, ["An error has occurred, and engineers have been informed. Please reload the page. If you continue to have problems, contact support@example.com\n"]]
-# end
-
-#if ENV['RAILS_ENV'] == 'production'
+if ENV['RAILS_ENV'] == 'production'
   directory "/home/ec2-user/apps/tipper/current"
   workers 2
   bind "unix:///home/ec2-user/apps/tipper/shared/tmp/sockets/puma.sock"
-  pidfile "/home/ec2-user/apps/tipper/current/tmp/pids/puma.pid"
-  state_path "/home/ec2-user/apps/tipper/current/tmp/pids/puma.state"
-  environment 'production'
-# else
-#   workers 1
-#   port 9888
-# end
+else
+  workers 1
+  port 9888
+end
 
 threads 1, 1
 
-#environment ENV['RAILS_ENV'] || 'development'
+environment ENV['RAILS_ENV'] || 'development'
 
 prune_bundler
-
-
