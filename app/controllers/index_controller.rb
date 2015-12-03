7c986e4e2337d5189a21bd912e74dc1ea66510a2
Ryan Romanchuk <rromanchuk@gmail.com>
2015-12-03 07:07:14 -0800

    allow index to be passed as param

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,33 +1,12 @@
 class IndexController < ApplicationController
   def index
-    #check_revision
     _index_text = get_index.gsub('CSRF-TOKEN', form_authenticity_token)
     render text: _index_text
   end
 
   private
-  def check_revision
-    @index_text = Redis.current.get("tipper:index:default")
-    # if rev != current_rev
-    #   @current_rev = rev
-    #   @index_text = Redis.current.get(current_rev)
-    # end
-  end
-
   def get_index
-    index_key = Redis.current.get("tipper:index:current")
+    index_key = params[:index_key] || Redis.current.get("tipper:index:current")
     Redis.current.get("tipper:index:#{index_key}")
-  end
-
-  # def rev
-  #   Redis.current.lindex("tipper", 0)
-  # end
-
-  def current_rev
-    @current_rev
-  end
-
-  def index_text
-    @index_text ||= Redis.current.get(current_rev)
   end
 end
