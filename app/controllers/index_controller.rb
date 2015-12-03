f6cca7a9a90ad45b951e201b07f99c62d23904f6
Ryan Romanchuk <rromanchuk@gmail.com>
2015-12-02 19:01:12 -0800

    new redis deployment

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,15 +7,16 @@ class IndexController < ApplicationController
 
   private
   def check_revision
-    if rev != current_rev
-      @current_rev = rev
-      @index_text = Redis.current.get(current_rev)
-    end
+    @index_text = Redis.current.get("tipper:index:default")
+    # if rev != current_rev
+    #   @current_rev = rev
+    #   @index_text = Redis.current.get(current_rev)
+    # end
   end
 
-  def rev
-    Redis.current.lindex("tipper", 0)
-  end
+  # def rev
+  #   Redis.current.lindex("tipper", 0)
+  # end
 
   def current_rev
     @current_rev
