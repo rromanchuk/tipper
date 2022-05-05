1689c5479f956d4d194a254b1595e1b26fc7c56e
Ryan Romanchuk <rromanchuk@gmail.com>
2022-05-04 19:48:43 -0700

    Prepare for public and archiving

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Tipper
+
+Abandoned 2015 project with [tipper-ios](https://github.com/rromanchuk/tipper-ios), [tipper-web](https://github.com/rromanchuk/tipper-web), and [tipper](https://github.com/rromanchuk/tipper)
+
+![TIPPER1](/tipper-1.png)
+![TIPPER2](/tipper-2.png)
+![TIPPER3](/tipper-3.png)
+![TIPPER4](/tipper-4.png)
+
 ## Deploy
 
 `bin/cap production deploy`
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ApplicationMailer
   
   def wallet_notify(tx)
     @tx = tx
-    mail(to: ['***REMOVED***', 'marcus.siegel@gmail.com', "ivan.kataitsev@gmail.com"], subject: "Wallet notify #{tx["category"]}: #{tx["amount"]}")
+    mail(to: ['rromanchuk@gmail.com', 'marcus.siegel@gmail.com', "ivan.kataitsev@gmail.com"], subject: "Wallet notify #{tx["category"]}: #{tx["amount"]}")
   end
 
 end
